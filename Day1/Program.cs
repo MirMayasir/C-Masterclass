64c636a490eece65c752b41790b6207cf1888286
Mir Mayasir <mayasirmir02@gmail.com>
2025-08-18 16:25:33 +0530

    initial commits

---
+++ Day1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //string myFriendsName = "Jannik";
+
+            //Console.WriteLine("my old friend is " + myFriendsName);
+
+            //myFriendsName = "Frank";
+
+            //Console.WriteLine("my new friend is " + myFriendsName);
+
+
+            ////user input
+            //Console.WriteLine("enter your input");
+            //string userInput = Console.ReadLine();
+
+            //Console.WriteLine("you entered " + userInput);
+
+
+            Console.WriteLine("Enter num1");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter num2");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = num1 + num2;
+
+            Console.WriteLine("The sum is " + sum);
+
+            //Value type -> Stores actual data
+            //eg -> int, string, null, struct, enum
+            //Stored in stack
+
+            //Reference Type
+            //It stores the memory address
+
+        }
+    }
+}
