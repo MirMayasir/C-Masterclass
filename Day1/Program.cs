f2da3bb1a468bd6c218f4718baa6b2c2d6a87bb7
Mir Mayasir <mayasirmir02@gmail.com>
2025-08-28 23:34:13 +0530

    Conditionals

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -80,6 +80,7 @@ namespace Day1
 
             string s1 = "Hello my name is \"mayasir\"";
             Console.WriteLine(s1);
+            Console.WriteLine(@"Hello my name is ""mayasir""");
 
         }
     }
