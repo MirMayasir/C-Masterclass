2a224351e836cd854470ecda3d91bb0d6dc22003
Mir Mayasir <mayasirmir02@gmail.com>
2025-09-02 22:36:27 +0530

    Conditionals

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -84,6 +84,7 @@ namespace Day1
             // Console.WriteLine("Hello my name is \\mayasir\\");
             // Console.WriteLine("Hello my name is \nmayasir");
             Console.WriteLine("Hello my name is \tmayasir");
+            Console.WriteLine("Hello my name is \bmayasir");
 
             Console.ReadLine();
 
