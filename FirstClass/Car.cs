51d05bb94b79f9343a61e390038b468ca0ebd6c1
Mir Mayasir <mayasirmir02@gmail.com>
2025-10-07 15:53:45 +0530

    Classes introduction

---
+++ FirstClass/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClass
+{ 
+    internal class Car
+    {
+        private string _model = "";
+        public string Model { get => _model;
+            set { if(String.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("You entered nothing");
+                    _model = "Default Value";
+                }
+                else
+                {
+                    _model = value;
+                }
+                }
+                } 
+        public Car(string model) {
+            Model = model;
+            Console.WriteLine("I have a " + Model + "  car");
+        }
+    }
+}
---
+++ FirstClass/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace FirstClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car audi = new Car("BMW");
+            Console.WriteLine("Main executed");
+
+            Console.WriteLine("enter the model name");
+
+            audi.Model = Console.ReadLine();
+
+            Console.WriteLine("you entered " +  audi.Model);
+
+            Console.ReadKey();
+        }
+    }
+}
